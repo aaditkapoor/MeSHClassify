31d7ae72b4d7ad98c9159bec64af0225e7849fa3
Aadit Kapoor <akapoorx00@gmail.com>
2022-07-05 11:13:27 -0700

    A

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if st.button("Classify Text"):
       y_hat = model(input_ids=input_ids.reshape(-1, 512),attention_mask = attention_mask.reshape(-1, 512))
       prob = F.softmax(y_hat, dim=1)
       probs = prob.detach().numpy()
-      my_bar = st.progress(probs[0])
+      my_bar = st.progress(probs[0][0])
 
       predictions = prob.argmax(dim=1)
       st.write(predictions.detach().numpy())
